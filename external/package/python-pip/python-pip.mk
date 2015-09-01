35c65597ccb6932cbf6ee9d406ff3118e4c931af
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-09-01 14:11:11 +0200

    Update pip to 7.1.2.

--- external/package/python-pip/python-pip.mk
+++ external/package/python-pip/python-pip.mk
@@ -4,7 +4,7 @@
 #
 #################################################################################
 
-PYTHON_PIP_VERSION = 7.1.0
+PYTHON_PIP_VERSION = 7.1.2
 PYTHON_PIP_SOURCE = pip-$(PYTHON_PIP_VERSION).tar.gz
 PYTHON_PIP_SITE = http://pypi.python.org/packages/source/p/pip/
 PYTHON_PIP_SETUP_TYPE = setuptools
